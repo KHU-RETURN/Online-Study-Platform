c7f606d966dcd6c2fca9a6228f4411c573cf22da
yeonjin <duswls214@khu.ac.kr>
2022-08-21 13:22:34 +0900

    add meeting posting to chat page

--- public/js/storage_detail.js
+++ public/js/storage_detail.js
@@ -3,6 +3,8 @@ var chatting_list;
 var confId = getParameter("id");
 init();
 
+document.querySelector("#meeting_title").readOnly = true;
+
 var toolbarOptions = [
   //toolbar 원하는 것 넣기
   ["bold", "italic", "underline", "strike"], // toggled buttons
@@ -18,22 +20,29 @@ var toolbarOptions = [
 ];
 
 function init() {
+  var quill = new Quill("#editor", {
+    modules: {
+      toolbar: toolbarOptions,
+    },
+    theme: "snow",
+  });
+  document.querySelector(".ql-toolbar").style.display = "none";
+  quill.enable(false); //보기만 가능 (viewer)
   // console.log(groupId, confId);
   fetch("/api/get_conference/" + confId)
     .then((response) => response.json())
     .then((response) => {
       var dateFormat = new Date(response.endTime);
-      dateFormat = (dateFormat.getFullYear()) + "년 " + (dateFormat.getMonth() + 1) + "월 " + dateFormat.getDate() + "일";
+      dateFormat =
+        dateFormat.getFullYear() +
+        "년 " +
+        (dateFormat.getMonth() + 1) +
+        "월 " +
+        dateFormat.getDate() +
+        "일";
       document.getElementById("date").innerHTML = dateFormat;
-      document.getElementById("title").innerHTML = response.title;
-      var quill = new Quill("#editor", {
-        modules: {
-          toolbar: toolbarOptions,
-        },
-        theme: "snow",
-      });
-      document.querySelector(".ql-toolbar").style.display = "none";
-      quill.enable(false); //보기만 가능 (viewer)
+      document.getElementById("meeting_title").value = response.title;
+
       // console.log(JSON.parse(JSON.stringify(response.record)));
       quill.setContents(JSON.parse(JSON.stringify(response.record)));
       document
@@ -56,7 +65,10 @@ function init() {
 
           data2.forEach((element) => {
             const chatDate = new Date(element.date);
-            if (chatDate.getTime() > startDate.getTime() && chatDate.getTime() < endDate.getTime()) {
+            if (
+              chatDate.getTime() > startDate.getTime() &&
+              chatDate.getTime() < endDate.getTime()
+            ) {
               // 불러온 데이터 시간순 아니면 -> 정렬하는 부분 추가
               if (userId === element.id) {
                 // owner이름과 같으면 이미지 오른쪽에 생성
@@ -69,36 +81,40 @@ function init() {
                 chatting.innerHTML += chat;
               }
             }
-
           });
         });
-    })
-
+    });
 }
 
 function editData(quill) {
   document.querySelector(".ql-toolbar").style.display = "block";
+  document.querySelector("#meeting_title").readOnly = false;
   document.querySelector("#modify_contents_button").textContent = "수정완료";
   quill.enable(); // 수정 가능
   document
     .querySelector("#modify_contents_button")
     .addEventListener("click", function () {
-      submitData(quill.getContents());
+      submitData(quill.getContents(), getTitle());
     });
 }
 
-function submitData(record_body) {
+function submitData(record_body, title) {
   // 서버에 전송
   // console.log(record_body);
-  fetch("/api/edit_conference/"+confId, {
+  fetch("/api/edit_conference/" + confId, {
     method: "PUT",
     body: JSON.stringify({
+      title,
       record: record_body,
     }),
     headers: {
       "content-type": "application/json;charset=utf-8",
     },
   }).then((response) => location.reload());
+}
+
+function getTitle() {
+  return document.querySelector("#meeting_title").value;
 }
 
 function getParameter(name) {
