b75371650b7f2babd0f8feafa335c72853180174
mynameSumin <msm4167@naver.com>
2022-08-23 05:43:04 +0900

    modify group page design

--- public/js/check_group.js
+++ public/js/check_group.js
@@ -8,6 +8,15 @@ var code = getParameter("code");
 const footer = document.getElementById("footer");
 const exit_button = document.getElementById("exit_group");
 let key;
+const r = document.querySelector(":root");
+/*
+console.log(location.href);
+temp = location.href.split("?");
+console.log(temp);
+data = temp[1].split("/");
+code = data[0];
+alert(code);
+*/
 
 init();
 function init() {
@@ -27,25 +36,18 @@ function loadGroup(id) {
     .then((data) => {
       if (data.length) {
         //배열이 넘어오면
-        original_value = data[0];
+        original_value = data[0]; //배열 데이터. 일치하는 모든 데이터가 불러와짐. 실제로 데이터 불러올 때는 하나만 가져올거니까 -> [0] 없애면 됨.
       } else {
         original_value = data;
       }
       group_name.innerText = original_value.name;
       description.innerText = original_value.description;
-      icons.forEach((element) => {
-        element.style.backgroundColor = original_value.color;
-        element.style.color = getTextColorByBackgroundColor(
+      for (let i = 0; i < icons.length; i++) {
+        icons[i].style.backgroundColor = original_value.color;
+        icons[i].style.color = getTextColorByBackgroundColor(
           original_value.color
         );
-      });
-      nav.style.backgroundColor = original_value.color;
-      nav_items.forEach((element) => {
-        element.style.borderColor = original_value.color;
-        element.style.color = getTextColorByBackgroundColor(
-          original_value.color
-        );
-      });
+      }
     });
 }
 
@@ -61,11 +63,11 @@ function getParameter(name) {
 function getTextColorByBackgroundColor(hexColor) {
   const c = hexColor.substring(1);
   const rgb = parseInt(c, 16);
-  const r = (rgb >> 16) & 0xff;
-  const g = (rgb >> 8) & 0xff;
-  const b = (rgb >> 0) & 0xff;
-  const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-  return luma < 127.5 ? "white" : "black";
+  const r = (rgb >> 16) & 0xff; // red 추출
+  const g = (rgb >> 8) & 0xff; // green 추출
+  const b = (rgb >> 0) & 0xff; // blue 추출
+  const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709     // 색상 선택
+  return luma < 127.5 ? "white" : "black"; // 글자색이}
 }
 
 function displayCode(id) {
